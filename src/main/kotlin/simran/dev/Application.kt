1c4816b22626840586002cc1f131800162082bbb
Simran Preet Singh Narang <jc@Simrans-MacBook-Pro.local>
2021-12-20 10:35:08 -0800

    first commit

---
+++ src/main/kotlin/simran/dev/Application.kt
@@ -0,0 +1,15 @@
+package simran.dev
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import simran.dev.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
