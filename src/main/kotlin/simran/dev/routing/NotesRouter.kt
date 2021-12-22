dfd9dae07248a23b8129dfc1a9c9c6cb305b3e1d
simransdsu <simran5590@gmail.com>
2021-12-22 14:57:55 -0800

    Creating Notes using POST request

--- src/main/kotlin/simran/dev/routing/NotesRouter.kt
+++ src/main/kotlin/simran/dev/routing/NotesRouter.kt
@@ -1,14 +1,19 @@
 package simran.dev.routing
 
 import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.ktorm.dsl.from
+import org.ktorm.dsl.insert
 import org.ktorm.dsl.map
 import org.ktorm.dsl.select
 import simran.dev.db.DatabaseConnection
 import simran.dev.entities.NoteEntity
 import simran.dev.models.Note
+import simran.dev.models.NoteRequest
+import simran.dev.models.NoteResponse
 
 fun Application.notesRoutes() {
     val db = DatabaseConnection.database
@@ -21,6 +26,20 @@ fun Application.notesRoutes() {
                 Note(id, note)
             }
             call.respond(notes)
+        }
+
+        post("/notes") {
+            val noteRequest = call.receive<NoteRequest>()
+
+            val result = db.insert(NoteEntity) {
+                set(NoteEntity.note, noteRequest.note)
+            }
+
+            if(result == 1) {
+                call.respond(HttpStatusCode.Created, NoteResponse(success = true, data = "Successfully created note"))
+                return@post
+            }
+            call.respond(HttpStatusCode.BadRequest, NoteResponse(success = false, data = "Failed to create a note"))
         }
     }
 }
